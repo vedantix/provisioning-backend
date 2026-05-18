2530b28246b2853e1d4799cca6d832cbdd15d268
Rishwi Jagesar <rishwijagesar@hotmail.com>
2026-05-18 19:37:45 +0200

    feat(api): add route registry

---
+++ src/routes/index.ts
@@ -0,0 +1,4 @@
+export { default as billingRoutes } from './billing.routes';
+export { default as adminRoutes } from './admin.routes';
+export { default as customerPortalRoutes } from './customer-portal.routes';
+export { default as webhooksRoutes } from './webhooks.routes';
