fa77875fb57a64db1acc0277b875402b4d12739c
Rishwi Jagesar <rishwijagesar@hotmail.com>
2026-05-18 13:54:25 +0200

    Export Migadu provider and provider factory

--- src/modules/mail/index.ts
+++ src/modules/mail/index.ts
@@ -3,6 +3,7 @@ export * from './package-rules';
 export * from './services/mail-domain.service';
 export * from './services/mailbox.service';
 export * from './services/mail-provisioning.service';
-export * from './providers/zoho-mail.provider';
+export * from './providers/migadu-mail.provider';
+export * from './providers/provider.factory';
 export { default as mailRoutes } from './routes/mail.routes';
 export { default as customerMailRoutes } from './routes/customer-mail.routes';
