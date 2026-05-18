8d0ea876a472527ef959d562fdd28251b0a61c0c
Rishwi Jagesar <rishwijagesar@hotmail.com>
2026-05-18 13:56:18 +0200

    Simplify mail provider factory to use Migadu only

--- src/modules/mail/providers/provider.factory.ts
+++ src/modules/mail/providers/provider.factory.ts
@@ -1,14 +1,6 @@
-import { mailConfig } from '../../../config/mail.config';
 import type { MailProvider } from './mail-provider.interface';
-import { ZohoMailProvider } from './zoho-mail.provider';
 import { MigaduMailProvider } from './migadu-mail.provider';
 
 export function createMailProvider(): MailProvider {
-  switch ((mailConfig.provider || 'ZOHO').toUpperCase()) {
-    case 'MIGADU':
-      return new MigaduMailProvider();
-    case 'ZOHO':
-    default:
-      return new ZohoMailProvider();
-  }
+  return new MigaduMailProvider();
 }
