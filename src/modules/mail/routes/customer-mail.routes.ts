2c3a2df302ca401a1f67a9c58bd7e313a010de33
Rishwi Jagesar <rishwijagesar@hotmail.com>
2026-05-18 14:26:20 +0200

    feat(mail): add mailbox usage route

--- src/modules/mail/routes/customer-mail.routes.ts
+++ src/modules/mail/routes/customer-mail.routes.ts
@@ -4,6 +4,7 @@ import { MailController } from '../controllers/mail.controller';
 const router = Router();
 const controller = new MailController();
 
+router.get('/:customerId/mail-usage', controller.getMailboxUsage);
 router.post('/:customerId/provision-mail', controller.provisionCustomerMail);
 
 export default router;
